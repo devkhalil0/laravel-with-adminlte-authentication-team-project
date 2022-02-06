2c56047269246aef11268c075929b7c7c1eb871d
khalilvaai <khalilvaai@yahoo.com>
2022-02-06 08:58:27 +0600

    api added

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return
+        $posts = Post::latest()->get();
+        // return response()->json(['posts' => $posts]);
+
+        return PostResource::collection($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'title' => 'required|string|min:2',
+            'description' => 'required|min:6'
+        ]);
+        // if validate fails
+        if ($validator->fails()) {
+
+            return response()->json(['errors' => $validator->errors()]);
+        }
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        return response()->json(['success' => 'Post Created !']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'title' => 'required|string|min:2',
+            'description' => 'required|min:6'
+        ]);
+        // if validate fails
+        if ($validator->fails()) {
+
+            return response()->json(['errors' => $validator->errors()]);
+        }
+
+        $post = Post::FindOrFail($id);
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        return response()->json(['success' => 'Post Updated !']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::FindOrFail($id);
+        $post->delete();
+        return response()->json(['success' => 'Post Deleted !']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Post Api
+Route::get('v1/post/index', [PostController::class,'index']);
+Route::post('v1/post/store', [PostController::class,'store']);
+Route::post('v1/post/update/{id}', [PostController::class,'update']);
+Route::delete('v1/post/destroy/{id}', [PostController::class,'destroy']);
